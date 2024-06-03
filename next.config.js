2c4bac67a24bedece1c487fadcc58e6476f024e0
SANDEEP KUMAR <143518095+skddl007@users.noreply.github.com>
2024-06-03 17:21:40 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
-module.exports = {
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+ 
+  Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  trailingSlash: true,
+ 
+  Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  skipTrailingSlashRedirect: true,
+ 
+  Optional: Change the output directory `out` -> `dist`
+  distDir: 'dist',
 }
+ 
+module.exports = nextConfig
