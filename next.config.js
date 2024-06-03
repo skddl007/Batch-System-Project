239e023cf02cf55140fd6fd4d8d5d99d55e69112
SANDEEP KUMAR <143518095+skddl007@users.noreply.github.com>
2024-06-03 17:09:29 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
-// next.config.js
-
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  reactStrictMode: true,
-    dir: 'build',
-    // Optional: configure other settings like assetPrefix, etc.
-  },
-};
-
-module.exports = nextConfig;
+  output: 'export',
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+ 
+module.exports = nextConfig
