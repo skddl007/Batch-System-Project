5f4abe9af9a53bdf668a276bdc25609e0f74dc4c
SANDEEP KUMAR <143518095+skddl007@users.noreply.github.com>
2024-06-03 17:14:36 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,8 @@
-/**
- * @type {import('next').NextConfig}
- */
 const nextConfig = {
-  output: 'export',
- 
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
- 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
- 
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
-}
- 
-module.exports = nextConfig
+  // ... other configurations
+  output: 'export', // Enable static export
+};
+output: {
+  export: 'export', // Enable static export
+  dir: 'my-custom-build-directory', // Optional output directory
+},
